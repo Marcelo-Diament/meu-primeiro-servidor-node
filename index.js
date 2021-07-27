5918220e442bc42112ec2a6fd23aa6c7ec96b281
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-26 21:22:39 -0300

    bugfix: adicionando charset ao Content-Type

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain'
+    'Content-Type': 'text/plain;charset=utf-8'
   })
   res.end('Servidor funcionando redondinho, ó!')
 }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
