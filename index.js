4d9f552963bdaa03ea89e3b56b23748f01c82cc2
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-26 21:24:55 -0300

    feature: alterando tipo de conteúdo para HTML

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const http = require('http')
 
 http.createServer((req, res) => {
+  // res.writeHead(200, {
+  //   'Content-Type': 'text/plain;charset=utf-8'
+  // })
+  // res.end('Servidor funcionando redondinho, ó!')
   res.writeHead(200, {
-    'Content-Type': 'text/plain;charset=utf-8'
+    'Content-Type': 'text/html;charset=utf-8'
   })
-  res.end('Servidor funcionando redondinho, ó!')
+  res.end('<h1>Servidor funcionando redondinho, ó!</h1>')
 }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
