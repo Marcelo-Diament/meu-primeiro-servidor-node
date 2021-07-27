f706f73e3af6ed2fe3c5411cd24d6e800e3f1cf4
Marcelo-Diament <marcelodiament@gmail.com>
2021-07-26 21:46:51 -0300

    feature: rotas com switch

--- index.js
+++ index.js
@@ -12,8 +12,23 @@ http.createServer((req, res) => {
   // })
   // res.end('<h1>Servidor funcionando redondinho, ó!</h1>')
   // CONTEÚDO COMO JSON
+  // res.writeHead(200, {
+  //   'Content-Type': 'application ld/json;charset=utf-8'
+  // })
+  // res.end('{"code":200,"message":"Servidor funcionando!"}')
+  // CONTROLANDO ROTAS
   res.writeHead(200, {
-    'Content-Type': 'application ld/json;charset=utf-8'
+    'Content-Type': 'text/html;charset=utf-8'
   })
-  res.end('{"code":200,"message":"Servidor funcionando!"}')
+  switch (req.url) {
+    case '/':
+      return res.end('<h1>VOCÊ ESTÁ NA HOME</h1>')
+    case '/sobre':
+      return res.end('<h1>VOCÊ ESTÁ NA PÁGINA SOBRE</h1>')
+    case '/produto':
+      return res.end('<h1>VOCÊ ESTÁ NA PÁGINA DE PRODUTO</h1>')
+    default:
+      return res.end(`<h1>NÃO EXISTE ESSA PÁGINA${req.url.toUpperCase().replace(/\/|-/g, ' ')}</h1>`)
+  }
+
 }).listen(3000, 'localhost', () => console.log('O servidor está rodando...'))
